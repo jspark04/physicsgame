7c3d7b604b6ec6b9ce2699ecd21044c5cf2b5b2c
John Park <johnpark.rice@gmail.com>
2026-04-12 15:49:21 -0700

    fix: set fire temperature in heating test (fire is always created hot)

--- src/materials/fire.test.ts
+++ src/materials/fire.test.ts
@@ -21,17 +21,9 @@ describe('updateFire', () => {
   });
   it('heats adjacent neighbors', () => {
     const g = new Grid(5, 5);
-    g.set(2, 2, MaterialType.FIRE); g.setLifetime(2, 2, 60);
+    g.set(2, 2, MaterialType.FIRE); g.setLifetime(2, 2, 60); g.setTemp(2, 2, 900);
     update(2, 2, g);
-    // Check corner cell (1,1) which is less likely to be moved into by fire
-    // Fire at (2,2) heats all 8 neighbors. Even with movement, corner (1,1) retains heat.
-    const allNeighbors = [
-      g.getTemp(1, 1), g.getTemp(2, 1), g.getTemp(3, 1),
-      g.getTemp(1, 2),                  g.getTemp(3, 2),
-      g.getTemp(1, 3), g.getTemp(2, 3), g.getTemp(3, 3),
-    ];
-    // At least one neighbor should be heated (not all will be if fire moves into some)
-    const heated = allNeighbors.some((t) => t > 20);
-    expect(heated).toBe(true);
+    expect(g.getTemp(2, 1)).toBeGreaterThan(20);
+    expect(g.getTemp(3, 2)).toBeGreaterThan(20);
   });
 });
